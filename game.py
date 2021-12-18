5c2f2a49f6c6e957af7365a723b35c8cdd1861fa
mrtony94 <tony.mst94@gmail.com>
2021-12-18 13:44:12 +0100

    eliminar comentarios de server.py

--- game.py
+++ game.py
@@ -171,8 +171,8 @@ class Game:
                         self.player_turn = 0  # poner la posición del turno a 0
                 else:
                     self.change_turn()
-                result += "Estp es linea 174"
-                result += self.play_enemies_turn()
+            result += "Estp es linea 174"
+            result += self.play_enemies_turn()
         elif command == "s":
             result += self.save_file(file_name)
         return result
